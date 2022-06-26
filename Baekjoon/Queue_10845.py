b8bb1edfd039c9555aed1b92d9403d2303b0de83
jemjemster <103993839+jemjemster@users.noreply.github.com>
2022-06-26 23:45:16 +0900

    Feat: Baekjoon new file

---
+++ Baekjoon/Queue_10845.py
@@ -0,0 +1,41 @@
+import sys                            
+from collections import deque           
+
+n = int(sys.stdin.readline())          
+queue = deque()                   
+for _ in range(n):                     
+    cmd = sys.stdin.readline().split()  
+    
+    if cmd[0]=="push":                  
+        queue.append(cmd[1])           
+    
+    elif cmd[0]=="pop":              
+        if len(queue)==0:               
+            print(-1)                 
+     
+        else:
+            print(queue.popleft())      
+                                       
+    elif cmd[0]=="size":
+        print(len(queue))              
+    
+    elif cmd[0]=="empty":
+        if len(queue)==0:              
+            print(1)
+    
+        else:
+            print(0)
+
+    elif cmd[0]=="front":     
+        if len(queue)==0:             
+            print(-1)
+
+        else:
+            print(queue[0])           
+
+    else:
+        if len(queue)==0:             
+            print(-1)
+
+        else:
+            print(queue[-1])  
