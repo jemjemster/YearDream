f088f9b69c50acb5149821e3db7a4e632ecc6b63
jemjemster <103993839+jemjemster@users.noreply.github.com>
2022-06-27 23:34:26 +0900

    Feat: Upload Stack file

---
+++ Baekjoon/Stack_10828.py
@@ -0,0 +1,33 @@
+import sys                                # sys 모듈
+n = int(sys.stdin.readline())             # 시간 단축  
+ 
+stack = []                                  
+for i in range(n):                     
+    cmd = sys.stdin.readline().split()  
+    
+    if cmd[0]=="push":                    
+        stack.append(cmd[1])           
+    
+    elif cmd[0]=="pop":              
+        if len(stack)==0:               
+            print(-1)                 
+     
+        else:
+            print(stack.pop())      
+                                       
+    elif cmd[0]=="size":
+        print(len(stack))              
+    
+    elif cmd[0]=="empty":
+        if len(stack)==0:              
+            print(1)
+    
+        else:
+            print(0)
+
+    elif cmd[0]=="top":     
+        if len(stack)==0:             
+            print(-1)
+
+        else:
+            print(stack[-1])           
